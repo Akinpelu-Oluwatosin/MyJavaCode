8e3c0865062e8b8c7c914bf0400dc57793c12fa6
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-10-22 11:23:32 +0100

    Update MyQueue.java

--- MyQueue.java
+++ MyQueue.java
@@ -1,31 +1,69 @@
 public class TheQueue {
-    String[] name = new String[5];
-    int front = 0;
-    int rear = 0;
+    public static void main(String[] args){
+    
+    String[] names = new String[5];
     int count = 0;
 
+   
     public boolean isEmpty() {
         return count == 0;
     }
 
-    public void AddQueue(String data) {
-        if (count < name.length) {
-            name[rear] = data;
-            rear = (rear + 1) % name.length;
+   
+    public void addQueue(String name) {
+
+         q.addQueue("Tayo");
+        q.addQueue("Tosin");
+        q.addQueue("Tope");
+
+        if (count < names.length) {
+            names[count] = name;
             count++;
+}
+        else {
+            System.out.println("Queue is full!");
+ }
+    }
+
+   
+    public String removeQueue() {
+        q.displayQueue();
+        q.removeQueue();
+        q.displayQueue();
+        
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return null;
+        }
+
+        String removed = names[0];
+    
+        for (int index = 0; index < count - 1; index++) {
+            names[index] = names[index + 1];
+        }
+
+        count--;
+        names[count] = null;
+        return removed;
+    }
+
+   
+    public void displayQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
         } else {
-            System.out.println("Queue Overflow");
+            System.out.print("Queue: ");
+            for (int count = 0; count < count; count++) {
+                System.out.print(names[count] + " ");
+            }
+            System.out.println();
         }
     }
 
-    public String RemoveQueue() {
-        if (isEmpty()) {
-            System.out.println("Queue is Empty");
-            return null;
-        }
-        String data = name[front];
-        front = (front + 1) % name.length;
-        count--;
-        return data;
+
+  
+        
+
     }
+}
 }
