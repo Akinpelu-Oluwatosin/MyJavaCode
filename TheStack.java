bce957f2c2859c7b9f442b32ae9078606d07aa11
Akinpelu Oluwatosin <fortifiedtosin@gmail.com>
2025-10-22 10:50:44 +0100

    Add files via upload

--- TheStack.java
+++ TheStack.java
@@ -8,18 +8,33 @@ public class TheStack {
     }
 
     public void push(String data) {
-        if (count < name.length) {
-            name[count++] = data;
-        } else {
-            System.out.println("Stack Overflow");
-        }
-    }
 
-    public String pop() {
+	name.push("Tayo");
+        name.push("Tosin");
+        name.push("Tope");
+
+        if (count < name.length) {
+            name[count++] = theElement;
+} 
+	
+	else {
+            System.out.println("Stack Overflow");
+}
+}
+
+    public String toRemove() {
+
+	name.pop("Tope");
+	name.pop("Tosin");
+	name.pop("Tope");
         if (isEmpty()) {
             System.out.println("Stack is Empty");
             return null;
-        }
-        return name[--count];
-    }
 }
+        return name[--count];
+}
+}
+
+
+
+
